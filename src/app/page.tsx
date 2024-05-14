accc907d9610d61cff9346ca6b42fb0014074a2a
MarkizOrias <georgi.bargan@gmail.com>
2024-05-14 16:29:14 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./Navbar";
+import Body from "./Body";
+
+export default function Home(): import("C:/Users/Georgi/dexyourmeme/node_modules/@types/react/ts5.0/index").JSX.Element {
+  return (
+
+    <Navbar />
+
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  );
+}
