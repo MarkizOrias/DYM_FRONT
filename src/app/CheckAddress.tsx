166f9e06364a1384b16e6c0429182fbbfb008c70
MarkizOrias <georgi.bargan@gmail.com>
2024-06-02 14:26:46 +0200

    removed check address

--- src/app/CheckAddress.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { useActiveAccount } from 'thirdweb/react';
-
-export const getWalletAddress = () => {
-    const activeAccount = useActiveAccount();
-    return activeAccount ? activeAccount.address : 'No wallet connected.';
-};
